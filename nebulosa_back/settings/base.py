a8c91e83249d8854ee49e252e87c93d8a6baee55
Stonoly <pierre.moly@gmail.com>
2025-01-29 20:51:31 +0100

    [chore] Adding the JWT REST Authetification 🪪
    Hop, hop, hop. We start to set up borders and customs 🛃
    
    - Adding Django Rest Framework
    - Adding Simple JWT
    - Update the `requirements.txt`

--- nebulosa_back/settings/base.py
+++ nebulosa_back/settings/base.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from datetime import timedelta
 
 # Répertoire de base
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -21,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -68,3 +71,27 @@ DATABASES = {
 # Configuration des fichiers statiques
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # Les utilisateurs doivent être authentifiés par défaut
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+}
